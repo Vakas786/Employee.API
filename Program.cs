773eedb2ac300fcb98ae1dbe02448cc80ae7c4ce
Vakash Hasan Khan <vakashasankhan@gmail.com>
2026-07-19 06:52:48 +0530

    Removed secrets from appsettings

--- Program.cs
+++ Program.cs
@@ -1,132 +1,120 @@
 using Asp.Versioning;
 using AutoMapper;
-using Azure.Identity;
 using Employee.API.Data;
 using Employee.API.Extensions;
 using Employee.API.Interfaces;
 using Employee.API.Mappings;
 using Employee.API.Repository;
 using Employee.API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
-using System.Text;
 using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
-//var keyVaultUrl = new Uri("https://vakas2026.vault.azure.net/");
-builder.Services.AddApplicationInsightsTelemetry(options =>
-{
-    options.ConnectionString =
-        builder.Configuration["ApplicationInsights:ConnectionString"];
-});
 
-//builder.Configuration.AddAzureKeyVault(
-//    keyVaultUrl,
-//    new DefaultAzureCredential());
+// ==========================================
+// Serilog
+// ==========================================
 Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .Enrich.FromLogContext()
     .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// ==========================================
+// Database
+// ==========================================
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// ==========================================
+// API Versioning
+// ==========================================
 builder.Services.AddApiVersioning(options =>
 {
     options.DefaultApiVersion = new ApiVersion(1, 0);
-
     options.AssumeDefaultVersionWhenUnspecified = true;
-
     options.ReportApiVersions = true;
-
-    options.ApiVersionReader =
-        new UrlSegmentApiVersionReader();
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
 });
 
+// ==========================================
+// Health Checks
+// ==========================================
 builder.Services.AddHealthChecks()
     .AddDbContextCheck<ApplicationDbContext>();
-builder.Host.UseSerilog();
+
+// ==========================================
+// Rate Limiter
+// ==========================================
 builder.Services.AddRateLimiter(options =>
 {
     options.AddFixedWindowLimiter("fixed", limiterOptions =>
     {
         limiterOptions.PermitLimit = 5;
-
         limiterOptions.Window = TimeSpan.FromMinutes(1);
-
         limiterOptions.QueueLimit = 0;
-
         limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
     });
 
     options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
 });
+
+// ==========================================
+// MVC & Swagger
+// ==========================================
 builder.Services.AddControllers();
-builder.Services.AddResponseCaching();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
+builder.Services.AddResponseCaching();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// ==========================================
+// Dependency Injection
+// ==========================================
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 builder.Services.AddScoped<IBlobStorageService, BlobStorageService>();
 builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 // ==========================================
-// JWT Authentication
+// Authorization
 // ==========================================
-
-//builder.Services.AddAuthentication(options =>
-//{
-//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-
-//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-//    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-//})
-//.AddJwtBearer(options =>
-//{
-//    options.RequireHttpsMetadata = false;
-
-//    options.SaveToken = true;
-
-//    //options.TokenValidationParameters = new TokenValidationParameters
-//    //{
-//    //    ValidateIssuer = true,
-
-//    //    ValidateAudience = true,
-
-//    //    ValidateLifetime = true,
-
-//    //    ValidateIssuerSigningKey = true,
-
-//    //    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-
-//    //    ValidAudience = builder.Configuration["Jwt:Audience"],
-
-//    //    IssuerSigningKey = new SymmetricSecurityKey(
-//    //        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-//    //};
-//});
-
 builder.Services.AddAuthorization();
 
+// ==========================================
+// Build App
+// ==========================================
 var app = builder.Build();
+
+// ==========================================
+// Middleware
+// ==========================================
 app.UseSerilogRequestLogging();
+
 app.UseGlobalExceptionMiddleware();
 
-app.UseSwagger();
+app.UseHttpsRedirection();
 
+app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseAuthentication();
+app.UseResponseCaching();
+
+app.UseRateLimiter();
+
+// Authentication is disabled for now
+// app.UseAuthentication();
 
 app.UseAuthorization();
-app.UseHttpsRedirection();
-app.UseResponseCaching();
-app.UseRateLimiter();
+
 app.MapControllers();
+
 app.MapHealthChecks("/health");
+
 app.Run();
